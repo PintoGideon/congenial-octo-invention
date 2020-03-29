ba506271901174261f92e3655c84c76dda437f96
PintoGideon <gideonpinto123@gmail.com>
2020-03-29 01:24:05 -0400

    Deleting a listing (Mutatation)in GraphQL

--- client/src/containers/Listings/Listings.tsx
+++ client/src/containers/Listings/Listings.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { server } from "../../lib/api/server";
-import { ListingsData } from "./types";
+import {
+  ListingsData,
+  DeleteListingData,
+  DeleteListingVariables
+} from "./types";
 
 interface Props {
   title: string;
@@ -22,6 +26,14 @@ query Listings {
 }
 `;
 
+const DELETE_LISTING = `
+mutation delete_listing($id:ID!){
+    deleteListing(id:$id){
+        id
+    }
+}
+`;
+
 export const Listings = ({ title }: Props) => {
   const fetchListings = async () => {
     const listings = await server.fetch<ListingsData>({
@@ -30,10 +42,26 @@ export const Listings = ({ title }: Props) => {
 
     console.log("Listings", listings);
   };
+
+  const deleteListing = async () => {
+    const listing = await server.fetch<
+      DeleteListingData,
+      DeleteListingVariables
+    >({
+      query: DELETE_LISTING,
+      variables: {
+        id: "5e7f51b380747c29e72880c8"
+      }
+    });
+
+    console.log("Delete_Listing", listing);
+  };
+
   return (
     <div>
       <h2>{title}</h2>
       <button onClick={fetchListings}>Query Listings</button>
+      <button onClick={deleteListing}>Delete a Listing</button>
     </div>
   );
 };
